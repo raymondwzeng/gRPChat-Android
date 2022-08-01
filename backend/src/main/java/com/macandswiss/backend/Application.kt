bd10fdf5d6d5659704175457928c6047b5513f87
raymondwzeng <rzeng3@gmail.com>
2022-07-31 23:22:14 -0700

    Initial backend setup

--- backend/src/main/java/com/macandswiss/backend/Application.kt
+++ backend/src/main/java/com/macandswiss/backend/Application.kt
@@ -1,4 +1,38 @@
 package com.macandswiss.backend
 
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
 class Application {
+    val server: Server = ServerBuilder.forPort(8080).addService(ChatService()).build()
+
+    internal class ChatService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
+    }
+
+    fun start() {
+        server.start()
+        println("Server started.")
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("Shutting down server...")
+                this@Application.stop()
+                println("Server shut down.")
+            }
+        )
+    }
+
+    fun stop() {
+        server.shutdown()
+    }
+
+    fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+
+}
+
+fun main() {
+    val server = Application()
+    server.start()
+    server.blockUntilShutdown()
 }
