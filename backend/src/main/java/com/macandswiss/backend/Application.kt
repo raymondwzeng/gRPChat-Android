c4a5e2f2b7c0f3bfa109844981a2fd3434eb82f7
raymondwzeng <rzeng3@gmail.com>
2022-08-05 22:38:51 -0700

    Updated backend to fill stubs

--- backend/src/main/java/com/macandswiss/backend/Application.kt
+++ backend/src/main/java/com/macandswiss/backend/Application.kt
@@ -2,11 +2,22 @@ package com.macandswiss.backend
 
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDateTime
 
 class Application {
     val server: Server = ServerBuilder.forPort(8080).addService(ChatService()).build()
 
     internal class ChatService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
+        override suspend fun getServerInformation(request: Grpchat.Nothing): Grpchat.ServerInformation {
+            return Grpchat.ServerInformation.newBuilder()
+                .setTopic("Hello from the server! The date and time is ${LocalDateTime.now()}!")
+                .build()
+        }
+
+        override fun chatBidirectional(requests: Flow<Grpchat.ChatToServer>): Flow<Grpchat.ChatToClient> {
+            return super.chatBidirectional(requests)
+        }
     }
 
     fun start() {
