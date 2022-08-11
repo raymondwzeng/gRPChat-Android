4cab954fb3a298260a6915fefb5701a653a770a1
raymondwzeng <rzeng3@gmail.com>
2022-08-10 22:34:44 -0700

    Updated protos, updated backend for new pub-sub-like model

--- backend/src/main/java/com/macandswiss/backend/Application.kt
+++ backend/src/main/java/com/macandswiss/backend/Application.kt
@@ -3,20 +3,27 @@ package com.macandswiss.backend
 import io.grpc.Server
 import io.grpc.ServerBuilder
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.LocalDateTime
 
 class Application {
     val server: Server = ServerBuilder.forPort(8080).addService(ChatService()).build()
-
     internal class ChatService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
+        private val chatFlow = MutableSharedFlow<Grpchat.ChatToClient>()
         override suspend fun getServerInformation(request: Grpchat.Nothing): Grpchat.ServerInformation {
             return Grpchat.ServerInformation.newBuilder()
                 .setTopic("Hello from the server! The date and time is ${LocalDateTime.now()}!")
                 .build()
         }
 
-        override fun chatBidirectional(requests: Flow<Grpchat.ChatToServer>): Flow<Grpchat.ChatToClient> {
-            return super.chatBidirectional(requests)
+        override suspend fun sendChat(request: Grpchat.ChatToServer): Grpchat.Nothing {
+            chatFlow.emit(Grpchat.ChatToClient.newBuilder().setAuthor("Unknown").setContent(request.content).build())
+            return Grpchat.Nothing.newBuilder().build()
+        }
+
+        override fun subscribeToChannel(request: Grpchat.Nothing): Flow<Grpchat.ChatToClient> {
+            return chatFlow
         }
     }
 
